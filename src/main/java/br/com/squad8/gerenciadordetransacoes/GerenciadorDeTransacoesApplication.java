98b5c0ca882896471be86469e119a26559b1d114
Chrystian Oliveira <chrystianmoliveira7@gmail.com>
2021-11-07 23:27:17 -0300

    iniciando escopo do projeto

---
+++ src/main/java/br/com/squad8/gerenciadordetransacoes/GerenciadorDeTransacoesApplication.java
@@ -0,0 +1,13 @@
+package br.com.squad8.gerenciadordetransacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeTransacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeTransacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/squad8/gerenciadordetransacoes/gerenciadordetransacoes/GerenciadorDeTransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.squad8.gerenciadordetransacoes.gerenciadordetransacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeTransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
